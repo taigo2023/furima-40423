b779c2eb1622c418ed3ecec21a85d43562c64f27
taigo2023 <taigo0617@gmail.com>
2024-05-26 20:47:31 +0900

    productsテーブルの作成

--- db/migrate/20240526112602_create_products.rb
+++ db/migrate/20240526112602_create_products.rb
@@ -1,7 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :category_id, null: false
+      t.integer :condition_id, null: false
+      t.integer :delivery_cost_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :delivery_day_id, null: false
+      t.integer :price, null: false
       t.timestamps
     end
   end
