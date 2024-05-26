b6b2c0a241e62f145d5acd765f2fc4952aa9d0b7
taigo2023 <taigo0617@gmail.com>
2024-05-26 18:25:51 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name
     validates :first_name
-
+  end
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
