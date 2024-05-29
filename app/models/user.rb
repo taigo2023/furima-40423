53a9c09ee902513764aecb9376feee655dbbd2a1
taigo2023 <taigo0617@gmail.com>
2024-05-29 20:12:04 +0900

    ActiveHashの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   validates :date_of_birth, presence: true
+  has_many :products
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' 
