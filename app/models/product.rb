54a5b6b03e5241358e96cabd2d90d0db0e23c21e
taigo2023 <taigo0617@gmail.com>
2024-05-29 21:11:00 +0900

    ActiveHash,商品モデルのアソシエーション

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_cost
+  belongs_to :prefecture
+  belongs_to :delivery_day
 end
