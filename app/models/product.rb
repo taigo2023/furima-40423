33714317e1f611627ea7c101dc3c69067be3352f
taigo2023 <taigo0617@gmail.com>
2024-05-29 22:44:27 +0900

    pruduct(商品)モデルのバリデーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,15 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_cost
   belongs_to :prefecture
   belongs_to :delivery_day
+
+  validates :name, :description, :price, presence: true
+
+  validates :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :delivery_day_id,numericality: { other_than: 1 , message: "can't be blank"} 
+
 end
