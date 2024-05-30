7aaffb39ba8caee5eee0c1d01b5fadd1c78c8854
taigo2023 <taigo0617@gmail.com>
2024-05-30 19:30:35 +0900

    商品出品機能　コントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @products = Product.order("created_at DESC")
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
   end
 end
