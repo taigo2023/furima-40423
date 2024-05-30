bebe1628d6274c1b5d568b84b599199f8af6c64d
taigo2023 <taigo0617@gmail.com>
2024-05-30 19:33:28 +0900

    商品出品機能　モデル

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,11 @@ class Product < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_day
 
-  validates :name, :description, :price, presence: true
+  validates :image, :name, :description, presence: true
+
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
 
   validates :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :delivery_day_id,numericality: { other_than: 1 , message: "can't be blank"} 
 
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :product do
-    
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph }
+    price { Faker::Commerce.price(range: 300..9999999) }
+    category_id { 2 }
+    condition_id { 2 }
+    delivery_cost_id { 2 }
+    prefecture_id { 2 }
+    delivery_day_id { 2 }
+    association :user
+
+    after(:build) do |product|
+      product.image.attach(io: File.open(Rails.root.join('public/images/test_image.png')), filename: 'test_image.png', content_type: 'image/png')
+    end
   end
 end
