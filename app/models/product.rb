fe967ac845603f02676f46d56e822141697d0428
taigo2023 <taigo0617@gmail.com>
2024-05-31 22:17:55 +0900

    商品出品機能　モデル修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,9 +10,7 @@ class Product < ApplicationRecord
 
   validates :image, :name, :description, presence: true
 
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-
-  validates :price, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
 
   validates :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :delivery_day_id,
             numericality: { other_than: 1, message: "can't be blank" }
