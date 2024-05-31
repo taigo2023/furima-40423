d4dc56d6c2b12a51409fdc87b55feca844480f7c
taigo2023 <taigo0617@gmail.com>
2024-05-31 22:18:11 +0900

    商品出品機能　Faker修正

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :product do
     name { Faker::Commerce.product_name }
     description { Faker::Lorem.paragraph }
-    price { Faker::Commerce.price(range: 300..9_999_999) }
+    price { Faker::Commerce.price(range: 300..9_999_999).to_i }
     category_id { 2 }
     condition_id { 2 }
     delivery_cost_id { 2 }
